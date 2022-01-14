790e95ae70931bdb86f9fcb1937d419da2ed6f23
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-14 19:30:40 +0530

    Add files via upload

---
+++ default constructor.cpp
@@ -0,0 +1,15 @@
+//default constructor
+#include<iostream>
+using namespace std;
+class Emp{
+	public:
+		Emp(){
+			
+			cout<<"default constructor is created"<<"\n";
+		}
+};
+int main(void){
+	Emp a1;
+	Emp a2;
+	return 0;
+}
