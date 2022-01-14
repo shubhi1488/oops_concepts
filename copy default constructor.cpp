e6c59ea653d73c6d692c68f15a66ba13e424cef3
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-14 19:29:21 +0530

    Add files via upload

---
+++ copy default constructor.cpp
@@ -0,0 +1,21 @@
+//copy constuctor-defualt constructor
+#include<iostream>
+using namespace std;
+class A{
+
+	public:
+		int x;
+		A(int a){
+			x=a;
+			
+		}
+		A(A &i){
+			x=i.x;
+		}
+};
+int main(){
+	A a1(20);
+	A a2(a1);
+	cout<<a2.x;
+	return 0;
+}
