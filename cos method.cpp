4bb16f32d71b96d1cf3741154101c0e255b6d52e
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-15 20:51:59 +0530

    Add files via upload

---
+++ cos method.cpp
@@ -0,0 +1,10 @@
+//math function-cos()
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	double degree=60;
+	double d=60*3.14/180;
+	cout<<"cosine of the angle is:"<<cos(d);
+	return 0;
+}
